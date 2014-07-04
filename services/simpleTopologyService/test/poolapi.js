cd2155c94e9d34a3ce171c2b9d22f42466f76d76
liuzcgithub <liuzc@cn.ibm.com>
2014-07-04 02:50:34 -0400

    update pool api tests

--- services/simpleTopologyService/test/poolapi.js
+++ services/simpleTopologyService/test/poolapi.js
@@ -28,26 +28,36 @@ var topologyHostname = nconf.get('HOSTNAME');
 var client = request.newClient('http://localhost:' + topologyPort);
 
 describe('SimpleTopologyService::Pool API v1', function() {
-    var newdata = {
+    var newpool = {
         name: 'test-pool-new' + today,
         description: 'pool of test topology instances of the latest build',
-        topologyRef: topologyHostname + ':' + topologyPort + '/topology/topologies/fakerecord',
+        topologyRef: '',
         poolMinAvailable: 0,
         poolMaxTotal: 2,
         type: 'noapp'
     };
-    var newdata_id;
+    var newpool_id;
+    var newtop = {
+      name: 'test-topology-new' + today,
+      description: 'testing a new topology creation update and deletion on ' + today,
+      referenceURL: 'http://jazz.net/testtopologynew'
+    };
+    var newtop_id;
     before(function(done) {
-        client.post('/api/v1/topology/pools', newdata, function(err, res, body) {
+      client.post('/api/v1/topology/topologies', newtop, function(err, res, body) {
+        newtop_id=body._id;
+        newpool.topologyRef=newtop_id;
+        client.post('/api/v1/topology/pools', newpool, function(err, res, body) {
             if (! err) {
                 assert.equal(res.statusCode, 200, 'Expected: 200 Actual: ' + res.statusCode);
                 assert.equal(
                 res.headers['content-type'],
                     'application/json; charset=utf-8',
                     'Expected: application/json; charset=utf-8 Actual: ' + res.headers['content-type']);
-                assert.equal(body.poolMethod, 'basic', 'Expected:basic,Actual:' + body.poolingMethod);
-                assert.equal(body.checkedOutInstances.length, 0, 'Expected empty array for checked out instances');
-                assert.equal(body.availableInstances.length, 0, 'Expected empty array for available instances');
+                assert.equal(body.poolMethod, 'basic', 'Expected:basic,Actual:' + body.poolMethod);
+                assert.equal(body.available, 0, 'Expected 0 available instances');
+                assert.equal(body.checkedout, 0, 'Expected 0 checked out instances');
+                newpool_id=body._id; 
                 done();
             }else {
                 console.log('SimpleTopologyService Pool API v1.before:could not create test data for pool');
@@ -56,20 +66,23 @@ describe('SimpleTopologyService::Pool API v1', function() {
                 done();
             }
         });
+        });
     });
     after(function(done) {
         remove = nconf.get('REMOVE-TEST-DATA');
         if (remove == 'false') {
             done();
         }else {
-            client.del('/api/v1/topology/pools/' + newdata.name, function(err, res, body) {
+            client.del('/api/v1/topology/pools/' + newpool_id, function(err, res, body) {
+            client.del('/api/v1/topology/topologies/' + newtop_id, function(err, res, body) {
                 done();
+            });
             });
         }
     });
     describe('GET /api/v1/topology/pools/:id', function() {
         it('should be able get created test data and a 200 response code and json', function(done) {
-            client.get('/api/v1/topology/pools/' + newdata.name, function(err, res, body) {
+            client.get('/api/v1/topology/pools/' + newpool_id, function(err, res, body) {
                 assert.equal(res.statusCode, 200, 'Expected: 200 Actual: ' + res.statusCode);
                 assert.equal(res.headers['content-type'], 'application/json; charset=utf-8',
                     'Expected: application/json; charset=utf-8 Actual: ' + res.headers['content-type']);
@@ -95,7 +108,7 @@ describe('SimpleTopologyService::Pool API v1', function() {
     });
   describe('POST /api/v1/topology/pools', function() {
         it('should return 400 response code if I try to create a pool with the same name', function(done) {
-            client.post('/api/v1/topology/pools', newdata, function(err, res, body) {
+            client.post('/api/v1/topology/pools', newpool, function(err, res, body) {
                 assert.equal(res.statusCode, 400, 'Expected: 400 Actual: ' + res.statusCode);
                 done();
             });
@@ -104,17 +117,11 @@ describe('SimpleTopologyService::Pool API v1', function() {
             var invalidPoolData = {
                         name: 'test-pool-new-invalid' + today,
                         description: 'invalid pool of test topology instances of the latest build',
-                        topologyRef: 'not a URL'
+                        topologyRef: 'not a topology id' 
                 };
                 client.post('/api/v1/topology/pools', invalidPoolData, function(err, res, body) {
                     assert.equal(res.statusCode, 400, 'Expected: 400 Actual: ' + res.statusCode);
-                    if (res.statusCode == 200) {
-                        client.del('/api/v1/topology/pools/' + invalidPoolData.name, function(err, res, body) {
-                            done();
-                        });
-                    }else {
                         done();
-                    }
                 });
         });
         it('should get 400 response code if attempt to set type to anything but latest and lastGood', function(done) {
@@ -122,7 +129,7 @@ describe('SimpleTopologyService::Pool API v1', function() {
                 name: 'test-pool-invalid-type' + today,
                 description: 'pool of test topology instances of the latest build',
                 topologyRef: 'http://www.google.com',
-                type: 'not last-good or latest'
+                type: 'not app or noapp'
             };
             client.post('/api/v1/topology/pools', invalidPoolDataType, function(err, res, body) {
                     assert.equal(res.statusCode, 400, 'Expected: 400 Actual: ' + res.statusCode);
