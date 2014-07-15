6f3c14c025854c7bd8d3699463f67b54a2772698
Zhi Cheng Liu <liuzc@cn.ibm.com>
2014-07-14 23:04:17 -0400

    add build management features

---
+++ services/simpleTopologyService/models/buildmodel.js
@@ -0,0 +1,41 @@
+/**
+*  Copyright 2014 IBM
+*
+*   Licensed under the Apache License, Version 2.0 (the "License");
+*   you may not use this file except in compliance with the License.
+*   You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+*   Unless required by applicable law or agreed to in writing, software
+*   distributed under the License is distributed on an "AS IS" BASIS,
+*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*   See the License for the specific language governing permissions and
+*   limitations under the License.
+*/
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/leanJazz',
+  function(err) {
+    if (!err) {
+      console.log('mongoose connected');
+    }else {
+      console.log('mongoose already connected');
+    }
+});
+
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+var BuildSchema = new Schema({
+	BUILDID: {type: String, unique: true},
+	buildStream: String,
+	refURL: String,
+	description: String,
+	isRecommened: {type: Boolean, default: false },	
+	},{strict: 'throw'}
+);
+
+var mBuild = mongoose.model('mBuild', BuildSchema);
+module.exports = mBuild;
+
+
